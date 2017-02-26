93e91c269bb1ee6330c55cf1e8ebe54794a251b2
Amol Disale <Alpha-Amol@users.noreply.github.com>
2017-02-27 04:49:31 +0530

    Add files via upload

---
+++ Esp8266 Code/HX711 Interfacing Code/HX711_Calibration_Code.ino
@@ -0,0 +1,51 @@
+#include "HX711.h"
+
+HX711 scale(12, 14);    // parameter "gain" is ommited; the default value 128 is used by the library
+
+float calibration_factor = 462.00; //460 worked for my 200gm scale setup
+int averageSamples = 100;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("HX711 Demo");
+  
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+  
+  Serial.println("Before setting up the scale:");
+  scale.set_scale(0);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  long zero_factor = scale.read_average(averageSamples); //Get a baseline reading
+  Serial.print("read average: \t\t");
+  Serial.println(zero_factor);    // print the average of 20 readings from the ADC
+  
+}
+
+void loop()
+{
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  
+  float weight = scale.getGram();
+
+  Serial.print("Reading:- ");
+  Serial.println(weight);
+  
+  Serial.print("calibration_factor:- ");
+  Serial.println(calibration_factor); 
+  
+  if (Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+       calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+        calibration_factor -= 10;
+  }
+  delay(2000);
+}
+
---
+++ Esp8266 Code/HX711 Interfacing Code/Measure_Weight_HX711.ino
@@ -0,0 +1,39 @@
+#include "HX711.h"
+
+HX711 scale(12, 14);    // parameter "gain" is ommited; the default value 128 is used by the library
+
+float calibration_factor = 461.00; //460 worked for my 200gm scale setup
+int averageSamples = 100;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("HX711 Demo");
+ 
+  Serial.println("Before setting up the scale:");
+  scale.set_scale(0);         // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+}
+
+void loop()
+{
+  float weight = 0.0, temp = 0.0;
+  for (int i = 0; i < 5; i++)
+  {
+    temp = scale.getGram();
+    if (temp < 0)
+    {
+      temp = 0;
+    }
+    weight = weight + temp;
+  }
+  weight = weight/5;
+  
+
+  Serial.print("Reading:- ");
+  Serial.print(weight);
+  Serial.println(" Gram");
+}
+
